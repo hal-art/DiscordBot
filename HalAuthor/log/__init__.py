9cea090ce913129d96a831b9990715c8ef7436f0
Hal <halcorder@gmail.com>
2023-04-22 20:50:11 +0900

    ログ出力先変更機能を追加した

--- HalAuthor/log/__init__.py
+++ HalAuthor/log/__init__.py
@@ -1,2 +1,3 @@
 from .log import log
 from .log import LogLevel
+from .log import set_log_dir
--- HalAuthor/log/log.py
+++ HalAuthor/log/log.py
@@ -13,7 +13,7 @@ log_file = os.path.join(log_dir, "example.log")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter("[%(asctime)s][%(levelname)s][%(name)s] %(message)s")
+formatter = logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s")
 
 # ファイル出力用のハンドラを追加
 file_handler = logging.FileHandler(log_file)
@@ -37,6 +37,18 @@ class LogLevel(Enum):
     ERROR = 3
     CRITICAL = 4
 
+def set_log_dir(dir:str):
+    """ログ出力先ディレクトリの変更
+
+    Args:
+        dir (str): ログ出力先
+    """
+    global log_dir
+    log_dir = dir
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    __log_reload()
+    
 def switch_log_mode(value:bool):
     """ログ出力モードを切り替える。
 
@@ -59,9 +71,7 @@ def log(msg:str, level:LogLevel):
     """
     global do_write_file
     
-    logtag = level.name
-    
-    __print_log(logtag, msg)
+    __print_log(level, msg)
     
     if do_write_file != True:
         return
@@ -81,12 +91,25 @@ def log(msg:str, level:LogLevel):
     elif level == LogLevel.CRITICAL:
         logger.critical(msg)
         
-def __print_log(logtag:str, msg:str):
+def __print_log(level:LogLevel, msg:str):
     """ログをコンソール出力する
 
     Args:
-        logtag (str): ログレベル
+        logtag (LogLevel): ログレベル
         msg (str): ログ出力文字列
     """
     datetime = get_now_time()
-    print(f"[{datetime}] level:{logtag} message:{msg}")
+    print(f"[{datetime}] level:{level.name} message:{msg}")
+    
+def __log_reload():
+    """ログ処理に必要な情報の更新
+    """
+    global file_handler, log_dir, log_file
+    
+    log_file = os.path.join(log_dir, "example.log")
+    new_file_handler = logging.FileHandler(log_file)
+    new_file_handler.setLevel(logging.DEBUG)
+    new_file_handler.setFormatter(formatter)
+    
+    logger.removeHandler(file_handler)
+    logger.addHandler(new_file_handler)
